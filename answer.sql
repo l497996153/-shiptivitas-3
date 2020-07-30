747a65f36a980bfbeefff92ccb5b5829328839c8
Zhaolong Liu <zhaolongliu@umass.edu>
2020-07-30 01:31:10 -0400

    Update answer.sql
    task3

--- answer.sql
+++ answer.sql
@@ -2,9 +2,12 @@
 
 -- PART 1: Create a SQL query that maps out the daily average users before and after the feature change
 
-select count(distinct(user_id)) as dailyAvgUsers,  DATE(login_timestamp, 'unixepoch') as loginDate from login_history where login_timestamp > strftime('%s','2018-06-02') group by loginDate
-union
+-- before
 select count(distinct(user_id)) as dailyAvgUsers,  DATE(login_timestamp, 'unixepoch') as loginDate from login_history where login_timestamp < strftime('%s','2018-06-02') group by loginDate;
+
+
+-- after
+select count(distinct(user_id)) as dailyAvgUsers,  DATE(login_timestamp, 'unixepoch') as loginDate from login_history where login_timestamp > strftime('%s','2018-06-02') group by loginDate;
 
 
 -- PART 2: Create a SQL query that indicates the number of status changes by card
